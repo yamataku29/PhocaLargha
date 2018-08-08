488ab02714be6636c921cf1f3ce4203617fcf26b
takuyama29 <tmn.sby001@gmail.com>
2018-08-08 14:21:06 +0900

    Initial Commit

---
+++ Example/Pods/Target Support Files/PhocaLargha/PhocaLargha-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PhocaLargha : NSObject
+@end
+@implementation PodsDummy_PhocaLargha
+@end
---
+++ Example/Pods/Target Support Files/Pods-PhocaLargha_Tests/Pods-PhocaLargha_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhocaLargha_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PhocaLargha_Tests
+@end
